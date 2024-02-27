efdcfb82afda1b9312259db5ee2c1fb655febcd0
Felipe Moreira <felipemoreira@gmail.com>
2024-02-27 10:19:59 -0300

    apostila02(git-python): funcao dividir implementada

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -2,6 +2,7 @@ import calculadora
 print (calculadora.somar(1, 2))
 print (calculadora.subtrair(1, 2))
 print (calculadora.multiplicar(1, 2))
+print (calculadora.dividir(1, 2))
 
 def main():
     a = 1
@@ -9,8 +10,11 @@ def main():
     soma = calculadora.somar(a, b)
     subtracao = calculadora.subtrair(a, b)
     produto = calculadora.multiplicar(a, b)
+    quociente = calculadora.dividir(a, b)
     print(f'{a} + {b} = {soma}')
     print(f'{a} - {b} = {subtracao}')
     print(f'{a} * {b} = {produto}')
+    print(f'{a} / {b} = {quociente}')
 
 main()
+
