60c0218a7a3c91626ed12a771711ee9840d47fec
Felipe Moreira <felipemoreira@gmail.com>
2024-02-27 10:01:38 -0300

    apostila02(git-python): teste da funcao multiplicar

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -1,3 +1,16 @@
 import calculadora
 print (calculadora.somar(1, 2))
 print (calculadora.subtrair(1, 2))
+print (calculadora.multiplicar(1, 2))
+
+def main():
+    a = 1
+    b = 2
+    soma = calculadora.somar(a, b)
+    subtracao = calculadora.subtrair(a, b)
+    produto = calculadora.multiplicar(a, b)
+    print(f'{a} + {b} = {soma}')
+    print(f'{a} - {b} = {subtracao}')
+    print(f'{a} * {b} = {produto}')
+
+main()
